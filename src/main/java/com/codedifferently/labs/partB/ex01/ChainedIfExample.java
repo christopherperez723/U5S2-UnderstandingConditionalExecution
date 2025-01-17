3c9994ded9818b03b3376c8e2150d034cf8c73d7
christopherperez723 <chris.l.perez723@gmail.com>
2025-01-17 18:55:23 -0500

    changes

--- src/main/java/com/codedifferently/labs/partB/ex01/ChainedIfExample.java
+++ src/main/java/com/codedifferently/labs/partB/ex01/ChainedIfExample.java
@@ -7,14 +7,14 @@ public class ChainedIfExample {
         double income = 30000, tax;
         if (income <= 15000) {
             tax = 0;
-
+            //if his income is less than or eqwal to 15000 his tax is zero
         } else if (income <= 25000) {
             tax = 0.05 * (income - 15000);
-
+            //else if his income is less than or eqwal to 25000 his tax is 0.05 * (income - 15000)
         } else {
             tax = 0.05 * (income - (25000 - 15000));
             tax += 0.10 * (income - 25000);
-
+            //else 
         }
         response += income + tax;
 
