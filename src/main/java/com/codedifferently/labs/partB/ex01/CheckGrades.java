e053706ae2264e3f90435bf15a78cc8a80ebbbf1
christopherperez723 <chris.l.perez723@gmail.com>
2025-01-25 11:56:59 -0500

    changes

--- src/main/java/com/codedifferently/labs/partB/ex01/CheckGrades.java
+++ src/main/java/com/codedifferently/labs/partB/ex01/CheckGrades.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class CheckGrades {
 
     public static String checkGrades() {
-        String response = "";
-        //if (Math === 80){
-        // return String = ""What was your grade in Math" + 80";
+        String response = "Math, Science, Geography, English, Java";
+        //if (Math === 80 || Science === 80){
+        // return String = ""What was your grade" + 80";
         // }else 
 
 
