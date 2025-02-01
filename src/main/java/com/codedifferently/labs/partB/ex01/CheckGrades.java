84355a88e8c09f6697ce4f64b7548f842d44105e
christopherperez723 <chris.l.perez723@gmail.com>
2025-02-01 11:53:15 -0500

    changes

--- src/main/java/com/codedifferently/labs/partB/ex01/CheckGrades.java
+++ src/main/java/com/codedifferently/labs/partB/ex01/CheckGrades.java
@@ -6,9 +6,14 @@ public class CheckGrades {
 
     public static String checkGrades() {
         String response = "Math, Science, Geography, English, Java";
+
+        String checkGrades = "Math, Science, Geography, English, Java";
+        int num = Integer.parseInt(checkGrades);
+        int adnum = Integer.parseInt("Math + Science + Geography + English + Java");
+        System.out.println("Average Grade is:" + "adnum");
+
         //if (Math === 80 || Science === 80 || Geography === 80 || English === 80 || Java === 80){
-        // return String = ""What was your grade" + 80";
-        // }else 
+        //return String = ""What was your grade" + 80";
 
 
         return response;
