6f1bade6faab48655615449d507cfaad8c19b16d
christopherperez723 <chris.l.perez723@gmail.com>
2025-02-04 18:56:12 -0500

    changes

--- src/main/java/com/codedifferently/labs/partB/ex02/Ternary.java
+++ src/main/java/com/codedifferently/labs/partB/ex02/Ternary.java
@@ -5,7 +5,11 @@ public class Ternary {
     public static String ternary(){
         String response = "";
         /* Put your code here */
-
+      String x = "1";
+      String y = "";
+      String question = x.equals("1") ? "Yes":"No";
+      //response += (x = 19 && y = 90);
+        //String question = x.equals("20") ? "Yes":"No";  
 
 
         return response;
