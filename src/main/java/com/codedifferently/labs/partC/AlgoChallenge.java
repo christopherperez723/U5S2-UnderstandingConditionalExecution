3a5ccccf6967823a7d7074d8bb63bf326facda0d
christopherperez723 <chris.l.perez723@gmail.com>
2025-02-28 18:57:30 -0500

    changes

--- src/main/java/com/codedifferently/labs/partC/AlgoChallenge.java
+++ src/main/java/com/codedifferently/labs/partC/AlgoChallenge.java
@@ -23,20 +23,16 @@ public class AlgoChallenge {
         
         return null;
     }
-
      /* Problem 2
     An elementary school teacher needs your help to create new strings for a fun activity they have planned.
      You will be provided a non-empty string and a number N. To create a new string, you will start with the character 0.
      Next you will add every Nth character of the string to the new string.
-
      Tip: If N is 3, use char 0, 3, 6 ... and so on. N is 1 or more.
-
      Example:
      everyOther("Miracle", 2) --> "Mrce"
      everyOther("abcdefg", 2) --> "aceg"
      everyOther("abcdefg", 3) --> "adg"
      */
-
     public static String everyOther(String str, int n) {
         String targets = String.valueOf(n);
         Boolean find = str.contains(targets);
@@ -49,5 +45,4 @@ public class AlgoChallenge {
         }
         return null;
     }
-
 }
