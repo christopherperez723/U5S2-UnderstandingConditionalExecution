3701389ac5d8175e513d11b2e1df6a1b233853dd
christopherperez723 <chris.l.perez723@gmail.com>
2025-03-07 18:57:49 -0500

    changes

--- src/main/java/com/codedifferently/labs/partB/ex02/TernaryOperatorExample.java
+++ src/main/java/com/codedifferently/labs/partB/ex02/TernaryOperatorExample.java
@@ -9,9 +9,7 @@ public class TernaryOperatorExample {
         String s2 = "Goodbye";
         String answer = s1.equals(s2) ? "Yes" : "No";
         response += (answer);
-
         return response;
-
     }
     public static void main(String args[]) {
         String output = tern();
